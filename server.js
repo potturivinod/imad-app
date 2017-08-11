85ca0e3bb49c3cc6d6e34eff00b9202eaeb11075
Vinod <30658827+potturivinod@users.noreply.github.com>
2017-08-11 17:44:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send(CreateTemplate(Articleone));
+  res.send(CreateTemplate(Acticleone));
 });
 
 app.get('/article-two', function (req, res) {
