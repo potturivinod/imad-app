093355fa866f3b27dba4fd62a9ff0a17cebffd1f
Vinod <30658827+potturivinod@users.noreply.github.com>
2017-08-17 22:40:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,8 +122,8 @@ app.get('/:ArticleName', function (req, res) {
 
 var counter = 0;
 app.get('/counter', function(req, res) {
-    counter = counter +1;
-    res.send(counter.tostring());
+    counter = counter + 1;
+    res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
